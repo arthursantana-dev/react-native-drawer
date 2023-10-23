0bd8614e5f8508bc7b83c07dbf403918cb53535b
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2023-10-23 12:31:03 -0300

    atualização dados.js

--- dados.js
+++ dados.js
@@ -3,15 +3,28 @@ const dados = [
 	
 	[
 		{
-			nome: "linguagem",
+			nome: "Linguagem, Trabalho e Tecnologia",
 			atribuicoesEResponsabilidades: "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e elaborar registros e planilhas de acompanhamento e controle de atividades",
-			valoresEAtitudes: "Incentivar o diálogo e a interlocução. Responsabilizar-se pela produção, utilização, e divulgação de informações e estimular a proatividade"
+			valoresEAtitudes: "• Incentivar o diálogo e a interlocução. • Responsabilizar-se pela produção, utilização, e divulgação de informações e estimular a proatividade"
 		},
 
 		{
-			nome: "programacao e algoritmos",
-			atribuicoesEResponsabilidades: "Implementar algoritmos em..."
-		}
+			nome: "Programação e Algoritmos",
+			atribuicoesEResponsabilidades: "Implementar algoritmos em linguagem de programação, utilizando ambientes de desenvolviemtnode acordo com as necessidades.",
+			valoresEAtitudes: "• Estimular a organização. 7• Incentivar atitudes de autonomia. • Fortalecer a persistência e o interesse na resolução de situações-problema."
+		},
+
+		{
+			nome: "Banco de Dados I",
+			atribuicoesEResponsabilidades: "Elaborar projetos de sistema de informação.",
+			valoresEAtitudes: "Estimular a organização. • Incentivar a criatividade. • Fortalecer a persi"
+		},
+
+		{
+			nome: "Análise e Projeto de Sistemas",
+			atribuicoesEResponsabilidades: "Elaborar projetos de sistema de informação.",
+			valoresEAtitudes: "Estimular a organização. • Incentivar a criatividade. • Fortalecer a persi"
+		},
 	],
 	
 	[
